a9615607df205c04cb9203fe4e8a4646bf928ddc
bego <oscar.bego@gmail.com>
2014-09-16 19:50:32 -0600

    Commit GET & POST demo
    SpackJava-demo, con AngularJs

---
+++ SparckDemo/src/app/model/Producto.java
@@ -0,0 +1,17 @@
+package app.model;
+
+public class Producto {
+
+    
+    public String nombre;
+    public String descri;
+    public double precio;
+
+    public Producto(String nombre, String descri, double precio) {
+        this.nombre = nombre;
+        this.descri = descri;
+        this.precio = precio;
+    }
+    
+    
+}
